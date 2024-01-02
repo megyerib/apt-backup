768f29891e067c2bd9f3b4c4de633d509e7928d7
Balázs Megyeri <megyerib@users.noreply.github.com>
2024-01-02 11:05:43 +0100

    /etc/apt backup created

--- update.sh
+++ update.sh
@@ -8,5 +8,9 @@ apt list --installed | grep -v "^Listing...$" > list.txt
 grep -oP "^[^/]+" list.txt > packets.txt
 grep "\[installed\]" list.txt | grep -oP "^[^/]+" > manual.txt
 
+# /etc/apt
+rm -rf ./etc_apt
+cp -r /etc/apt ./etc_apt
+
 # Snap
 snap list | sed -E "s/\s+/,/g" > snap.csv
